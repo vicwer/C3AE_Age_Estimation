000f3f67cc1a169531215c534068768cf5a453f4
K.G.Duan <vicwer@users.noreply.github.com>
2019-06-17 00:42:27 +0800

    add OHEM

--- models/losses.py
+++ models/losses.py
@@ -7,10 +7,18 @@ sys.path.append('..')
 from config import cfg
 
 def l1_loss(preds, labels):
-    return tf.reduce_sum(tf.abs(labels - preds))
+    preds = tf.reshape(preds, (-1,))
+    labels = tf.reshape(labels, (-1,))
+    l1_loss = tf.abs(labels - preds)
+    _, k_idx = tf.nn.top_k(l1_loss, tf.cast(tf.reduce_prod(l1_loss.get_shape()) * cfg.ohem_ratio), tf.int32)
+    loss = tf.gather(l1_loss, k_idx)
+    return tf.reduce_mean(loss)
 
 def kl_loss(preds, labels, l1):
-    # h_pq = -(tf.reduce_sum(tf.reduce_sum(labels * tf.log(preds + 1e-10)), axis=1))
+    # h_pq = -(tf.reduce_sum(tf.reduce_sum(labels * tf.log(preds + 1e-10), axis=1)))
     # h_p = -tf.reduce_sum(labels * tf.log(labels + 1e-10))
     # loss = h_pq - h_p + l1
-    return -(tf.reduce_sum(tf.reduce_sum(labels * tf.log(preds + 1e-10)), axis=1)) + l1
+    kl_loss = tf.reduce_sum(labels * tf.log(preds + 1e-10), axis=1)
+    _, k_idx = tf.nn.top_k(kl_loss, tf.cast(tf.reduce_prod(kl_loss.get_shape()) * cfg.ohem_ratio), tf.int32)
+    loss = tf.gather(kl_loss, k_idx)
+    return -tf.reduce_mean(loss) + l1
